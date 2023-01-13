1ba91811d1eab476a78df6045e582d490eacf362
Lucas Raimundo <lucasraimundo2010@gmail.com>
2023-01-13 13:31:46 -0300

    Find by ID endpoint

--- src/main/java/com/lucasraimundo/attonatus/resource/ClienteResource.java
+++ src/main/java/com/lucasraimundo/attonatus/resource/ClienteResource.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,14 @@ public class ClienteResource {
 		List<Cliente> list = service.findAll();
 		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());  
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
+		
+		Cliente obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+		
 	}
 
 }
--- src/main/java/com/lucasraimundo/attonatus/service/ClienteService.java
+++ src/main/java/com/lucasraimundo/attonatus/service/ClienteService.java
@@ -1,7 +1,9 @@
 package com.lucasraimundo.attonatus.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,4 +19,13 @@ public class ClienteService {
 	public List<Cliente> findAll(){
 		return repo.findAll();
 		}
+	
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName(), obj));
+	}
+	
+	
+	
 }
