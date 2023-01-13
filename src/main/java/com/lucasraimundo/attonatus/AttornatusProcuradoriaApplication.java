22e93be9de25d5cbe3e4656cb3f9e292ee65e05a
Lucas Raimundo <lucasraimundo2010@gmail.com>
2023-01-13 12:06:53 -0300

    Project created

---
+++ src/main/java/com/lucasraimundo/attonatus/AttornatusProcuradoriaApplication.java
@@ -0,0 +1,13 @@
+package com.lucasraimundo.attonatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttornatusProcuradoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttornatusProcuradoriaApplication.class, args);
+	}
+
+}
