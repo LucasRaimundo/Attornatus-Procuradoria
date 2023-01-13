614fb71a931b685c812602d050facd4ba64c3da6
Lucas Raimundo <lucasraimundo2010@gmail.com>
2023-01-13 12:27:57 -0300

    Creating Cliente and configuration h2 database

---
+++ src/main/java/com/lucasraimundo/attonatus/domain/Cliente.java
@@ -0,0 +1,74 @@
+package com.lucasraimundo.attonatus.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cliente implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String nasc;
+	
+	public Cliente() {
+		
+	}
+
+	public Cliente(Integer id, String nome, String nasc) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.nasc = nasc;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNasc() {
+		return nasc;
+	}
+
+	public void setNasc(String nasc) {
+		this.nasc = nasc;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
